7db0aed7a38c56615a7271b999d74b779501688e
longit-git <physicist.ylongxin@gmail.com>
2026-02-28 17:58:45 +0800

    Update Gaussianity_chi2.m

--- lib-power-grid/Gaussianity_chi2.m
+++ lib-power-grid/Gaussianity_chi2.m
@@ -1,42 +1,54 @@
-function [chi2_stat, sigma_hat, df] = Gaussianity_chi2(data, num_bin)
-% data: n x column
-% num_bin: number of bins (k)
-% chi2_stat: 1 x column chi-square statistic
-% sigma_hat: 1 x column fitted sigma
-% df: 1 x column degrees of freedom (approx k-3, after filtering bins)
+function [chi2_stat, r2, rmse] = Gaussianity_chi2(data, num_bin)
+% chi2 = sum( (model - y).^2 ./ model )
+% model: Gaussian pdf
+% y: empirical pdf (histogram)
 
-    [n, column] = size(data);
+    [~, column] = size(data);
+
     chi2_stat = nan(1, column);
     sigma_hat = nan(1, column);
-    df = nan(1, column);
+    r2        = nan(1, column);
+    rmse      = nan(1, column);
 
     for fit_node = 1:column
         x = data(:, fit_node);
 
-        % Fit Normal(mu, sigma)
+        % ===== 1. Fit Gaussian =====
         pd = fitdist(x, 'Normal');
-        sigma_hat(fit_node) = pd.sigma;
+        mu    = pd.mu;
+        sigma = pd.sigma;
+        sigma_hat(fit_node) = sigma;
 
-        % Histogram counts (NOT pdf)
-        [O, edges] = histcounts(x, num_bin, 'Normalization', 'count');
+        % ===== 2. Empirical pdf =====
+        [y, edges] = histcounts(x, num_bin, 'Normalization', 'pdf');
+        bin_centers = (edges(1:end-1) + edges(2:end)) / 2;
 
-        % Expected counts from fitted CDF over bin edges
-        p = cdf(pd, edges(2:end)) - cdf(pd, edges(1:end-1));   % 1 x k
-        E = n * p;                                             % 1 x k
+        % ===== 3. Gaussian model (pdf) =====
+        model = pdf(pd, bin_centers);
 
-        % Remove bins with E=0 (or too small)
-        valid = E > 0;   % you can tighten to E >= 5 if you want
-        O = O(valid);
-        E = E(valid);
+        % ===== 4. valid bins =====
+        valid = model > 0;
+        y     = y(valid);
+        model = model(valid);
 
-        % Pearson chi-square
-        chi2 = sum((O - E).^2 ./ E);
+        n = numel(y);
 
-        % degrees of freedom: (#valid bins) - 1 - (#estimated params=2)
-        k_eff = numel(E);
-        df_eff = k_eff - 1 - 2;
+        % ===== 5. χ² / R² / RMSE =====
+        y_mean = mean(y);
 
+        ss_res = 0.0;
+        ss_tot = 0.0;
+        chi2   = 0.0;
+
+        for i = 1:n
+            residual = model(i) - y(i);
+            ss_res = ss_res + residual^2;
+            ss_tot = ss_tot + (y(i) - y_mean)^2;
+            chi2   = chi2   + residual^2 / model(i);
+        end
+
+        r2(fit_node)   = 1.0 - ss_res / ss_tot;
+        rmse(fit_node) = sqrt(ss_res / n);
         chi2_stat(fit_node) = chi2;
-        df(fit_node) = df_eff;
     end
 end
